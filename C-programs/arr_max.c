d48e448b0bd27fc75b106a97e220174ec0d42ccb
Ramisettykumar <137613636+Ramisettykumar@users.noreply.github.com>
2024-02-15 11:36:53 +0530

    Add files via upload

---
+++ C-programs/arr_max.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void max()
+{
+	int i,a[10];
+	printf("Enter the ten numbers: \n");
+	for(i=0;i<10;i++)
+	scanf("%d",&a[i]);
+	for(i=0;i<10;i++)
+	{
+		if(a[0]<a[i])
+			a[0]=a[i];
+	}
+	printf("The max element in array is %d\n",a[0]);
+}
+int main()
+{
+	max();
+}
+
