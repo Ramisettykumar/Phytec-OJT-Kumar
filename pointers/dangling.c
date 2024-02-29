9dae735a7ccfbccba72e16faf1e756fd0038a401
Ramisettykumar <137613636+Ramisettykumar@users.noreply.github.com>
2024-02-29 10:19:34 +0530

    Delete pointers directory

--- pointers/dangling.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>  
-int main()  
-{  
-   int *ptr=(int *)malloc(sizeof(int));  
-   int a=560;  
-   ptr=&a;  
-   free(ptr);  
-   return 0;  
-}  
--- pointers/nullpon.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int* ptr = NULL;
-    printf("The value of ptr is %p", ptr);
-    return 0;
-}
